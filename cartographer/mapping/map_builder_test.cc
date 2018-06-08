2f45360665573d3f2cc6d35c910e857463f5262a
Andre Gaschler <gaschler@google.com>
2018-06-08 18:04:31 +0200

    Test Load/SerializeState and global localization

--- cartographer/mapping/map_builder_test.cc
+++ cartographer/mapping/map_builder_test.cc
@@ -15,11 +15,12 @@
  */
 
 #include "cartographer/mapping/map_builder.h"
-#include <gmock/gmock-matchers.h>
 
 #include "cartographer/common/config.h"
+#include "cartographer/io/proto_stream.h"
 #include "cartographer/mapping/internal/testing/test_helpers.h"
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 namespace cartographer {
 namespace mapping {
@@ -41,6 +42,8 @@ class MapBuilderTest : public ::testing::Test {
       include "map_builder.lua"
       MAP_BUILDER.use_trajectory_builder_2d = true
       MAP_BUILDER.pose_graph.optimize_every_n_nodes = 0
+      MAP_BUILDER.pose_graph.global_sampling_ratio = 0.05
+      MAP_BUILDER.pose_graph.global_constraint_search_after_n_seconds = 0
       return MAP_BUILDER)text";
     auto map_builder_parameters = test::ResolveLuaParameters(kMapBuilderLua);
     map_builder_options_ =
@@ -265,7 +268,7 @@ TEST_F(MapBuilderTest, GlobalSlam3D) {
               0.1 * kTravelDistance);
 }
 
-TEST_F(MapBuilderTest, DeleteTrajectory2D) {
+TEST_F(MapBuilderTest, DeleteFinishedTrajectory2D) {
   SetOptionsEnableGlobalOptimization();
   BuildMapBuilder();
   int trajectory_id = CreateTrajectoryWithFakeData();
@@ -301,6 +304,131 @@ TEST_F(MapBuilderTest, DeleteTrajectory2D) {
       map_builder_->pose_graph()->GetAllSubmapData().SizeOfTrajectoryOrZero(
           another_trajectory_id),
       0);
+}
+
+TEST_F(MapBuilderTest, SaveLoadState2D) {
+  BuildMapBuilder();
+  int trajectory_id = CreateTrajectoryWithFakeData();
+  map_builder_->pose_graph()->RunFinalOptimization();
+  int num_constraints = map_builder_->pose_graph()->constraints().size();
+  int num_nodes =
+      map_builder_->pose_graph()->GetTrajectoryNodes().SizeOfTrajectoryOrZero(
+          trajectory_id);
+  EXPECT_GT(num_constraints, 0);
+  EXPECT_GT(num_nodes, 0);
+  // TODO(gaschler): Consider using in-memory to avoid side effects.
+  const std::string filename = "temp-SaveLoadState2D.pbstream";
+  io::ProtoStreamWriter writer(filename);
+  map_builder_->SerializeState(&writer);
+  writer.Close();
+
+  // Reset MapBuilder.
+  BuildMapBuilder();
+  io::ProtoStreamReader reader(filename);
+  map_builder_->LoadState(&reader, false /* load_frozen_state */);
+  // TODO(gaschler): Design better way to find out which new trajectory_ids were
+  // created by LoadState.
+  map_builder_->pose_graph()->RunFinalOptimization();
+  auto nodes = map_builder_->pose_graph()->GetTrajectoryNodes();
+  ASSERT_GT(nodes.size(), 0);
+  int new_trajectory_id = nodes.begin()->id.trajectory_id;
+  EXPECT_EQ(num_constraints, map_builder_->pose_graph()->constraints().size());
+  EXPECT_EQ(
+      num_nodes,
+      map_builder_->pose_graph()->GetTrajectoryNodes().SizeOfTrajectoryOrZero(
+          new_trajectory_id));
+}
+
+TEST_F(MapBuilderTest, LocalizationOnFrozenTrajectory2D) {
+  BuildMapBuilder();
+  int temp_trajectory_id = CreateTrajectoryWithFakeData();
+  map_builder_->pose_graph()->RunFinalOptimization();
+  EXPECT_GT(map_builder_->pose_graph()->constraints().size(), 0);
+  EXPECT_GT(
+      map_builder_->pose_graph()->GetTrajectoryNodes().SizeOfTrajectoryOrZero(
+          temp_trajectory_id),
+      0);
+  const std::string filename = "temp-LocalizationOnFrozenTrajectory2D.pbstream";
+  io::ProtoStreamWriter writer(filename);
+  map_builder_->SerializeState(&writer);
+  writer.Close();
+
+  // Reset MapBuilder.
+  local_slam_result_poses_.clear();
+  SetOptionsEnableGlobalOptimization();
+  BuildMapBuilder();
+  io::ProtoStreamReader reader(filename);
+  map_builder_->LoadState(&reader, true /* load_frozen_state */);
+  map_builder_->pose_graph()->RunFinalOptimization();
+  // TODO(gaschler): Consider loading while SLAMing.
+  int trajectory_id = map_builder_->AddTrajectoryBuilder(
+      {kRangeSensorId}, trajectory_builder_options_,
+      GetLocalSlamResultCallback());
+  TrajectoryBuilderInterface* trajectory_builder =
+      map_builder_->GetTrajectoryBuilder(trajectory_id);
+  // TODO(gaschler): Add some yaw rotation.
+  // transform::Rigid3d frozen_trajectory_to_global(Eigen::Vector3d(2.0, 1.0, 0), rotation);
+  Eigen::Quaterniond rotation(
+      Eigen::AngleAxisd(0.5, Eigen::Vector3d::UnitZ()));
+  transform::Rigid3d frozen_trajectory_to_global =
+      transform::Rigid3d::Translation(Eigen::Vector3d(2.0, 1.0, 0));
+  auto measurements = test::GenerateFakeRangeMeasurements(kTravelDistance,
+                                                          kDuration, kTimeStep);
+  double delay_measurements = 100.;
+  for (auto& measurement : measurements) {
+    measurement.time += common::FromSeconds(delay_measurements);
+    measurement.ranges = sensor::TransformTimedPointCloud(
+        measurement.ranges,
+        frozen_trajectory_to_global.cast<float>().inverse());
+    trajectory_builder->AddSensorData(kRangeSensorId.id, measurement);
+  }
+  map_builder_->FinishTrajectory(trajectory_id);
+  map_builder_->pose_graph()->RunFinalOptimization();
+  EXPECT_EQ(local_slam_result_poses_.size(), measurements.size());
+  EXPECT_NEAR(kTravelDistance,
+              (local_slam_result_poses_.back().translation() -
+               local_slam_result_poses_.front().translation())
+                  .norm(),
+              0.1 * kTravelDistance);
+  EXPECT_GE(map_builder_->pose_graph()->constraints().size(), 50);
+  auto constraints = map_builder_->pose_graph()->constraints();
+  int num_cross_trajectory_constraints = 0;
+  for (const auto& constraint : constraints) {
+    if (constraint.node_id.trajectory_id !=
+        constraint.submap_id.trajectory_id) {
+      num_cross_trajectory_constraints++;
+    }
+  }
+  EXPECT_GT(num_cross_trajectory_constraints, 0);
+  // TODO(gaschler): Subscribe global slam callback, verify that all nodes are
+  // optimized.
+  EXPECT_THAT(constraints, testing::Contains(testing::Field(
+                               &PoseGraphInterface::Constraint::tag,
+                               PoseGraphInterface::Constraint::INTER_SUBMAP)));
+  const auto trajectory_nodes =
+      map_builder_->pose_graph()->GetTrajectoryNodes();
+  EXPECT_GE(trajectory_nodes.SizeOfTrajectoryOrZero(trajectory_id), 20);
+  const auto submap_data = map_builder_->pose_graph()->GetAllSubmapData();
+  EXPECT_GE(submap_data.SizeOfTrajectoryOrZero(trajectory_id), 5);
+  const transform::Rigid3d global_pose =
+      map_builder_->pose_graph()->GetLocalToGlobalTransform(trajectory_id) *
+      local_slam_result_poses_.back();
+  EXPECT_GT(map_builder_->pose_graph()
+                ->GetLocalToGlobalTransform(trajectory_id)
+                .translation()
+                .norm(),
+            1.0);
+  Eigen::Vector3d travel_translation =
+      Eigen::Vector3d(2., 1., 0.).normalized() * kTravelDistance;
+  const transform::Rigid3d expected_global_pose =
+      frozen_trajectory_to_global *
+      transform::Rigid3d::Translation(travel_translation);
+  EXPECT_NEAR(
+      0.,
+      (global_pose.translation() - expected_global_pose.translation()).norm(),
+      0.2) << "global_pose: " << global_pose << "expected_global_pose: " << expected_global_pose;
+  LOG(INFO) << "num_cross_trajectory_constraints: "
+            << num_cross_trajectory_constraints;
 }
 
 }  // namespace
